a5bc6382f6d05723bcd0d5c6c1edc3017d3df595
synnomar <synnomar@hotmail.com>
2026-03-28 10:19:30 -0300

    Atualização aula 7

--- Aula7/js/script.js
+++ Aula7/js/script.js
@@ -2,7 +2,10 @@ const nome = prompt('Digite seu nome completo: ');
 document.body.innerHTML += `O seu nome é: ${nome} <br />`;
 document.body.innerHTML += `O seu nome tem ${nome.length} letras <br />`;
 document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]} <br />`;
-document.body.innerHTML += `Qual o primeiro índice da letra S no seu nome? ${nome.indexOf()}<br />`;
-document.body.innerHTML += ` Qual o último índice da letra R no seu nome? ${nome.lastIndexOf()} <br />`;
-document.body.innerHTML += ` <br />`;
-document.body.innerHTML += ` <br />`;
+document.body.innerHTML += `Qual o primeiro índice da letra S no seu nome? ${nome.indexOf('S')}<br />`;
+document.body.innerHTML += ` Qual o último índice da letra R no seu nome? ${nome.lastIndexOf('r')} <br />`;
+document.body.innerHTML += ` As últimas 3 letras do seu nome são: ${nome.slice(-3)}<br />`;
+document.body.innerHTML += ` As palavras do seu nome são: ${nome.split(' ')}<br />`;
+document.body.innerHTML += ` O seu nome em letras maiúsculas é: ${nome.toUpperCase()}<br />`;
+document.body.innerHTML += ` O seu nome em letras minúsculas é: ${nome.toLowerCase()}<br />`;
+
