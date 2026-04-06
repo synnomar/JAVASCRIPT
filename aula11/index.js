8ff968a9287abaea5fc56c9071c04e1e4c5d9a10
synnomar <synnomar@hotmail.com>
2026-04-06 12:24:11 -0300

    aula

--- aula11/index.js
+++ aula11/index.js
@@ -1,10 +1,16 @@
-function soma(x,y){
-    const resutado = x + y;
-    return resutado
-}
+// function soma(x,y){
+//     const resutado = x + y;
+//     return resutado
+// }
 
-console.log(soma(2,2));
-console.log(soma(3,1));
-console.log(soma(5,10));
+// console.log(soma(2,2));
+// console.log(soma(3,1));
+// console.log(soma(5,10));
 
 
+//const raiz = function (n){return n ** 0.5};
+const raiz = n => n ** 0.5;
+
+console.log(raiz(9))
+console.log(raiz(16))
+console.log(raiz(25))
